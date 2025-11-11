c3f75fa8cabc8524d7dd512770860e02f0a56101
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-11 14:19:11 +0900

    feat(PlayerCharacter) : 레벨업 로직 추가
    - 레벨 저장 메서드 추가
    - 레벨업 메서드 추가

---
+++ src/main/model/unit/character/PlayerCharacter.java
@@ -0,0 +1,77 @@
+package main.model.unit.character;
+
+import java.util.ArrayList;
+import java.util.List;
+import main.model.unit.Unit;
+import main.model.unit.character.job.firstClass.SwordMan;
+
+public class PlayerCharacter extends Unit {
+    private int level;
+    private int exp;
+    private int maxExp;
+    private int gold;
+    private CharacterJob currentJob;
+    private final int levelUpPlusStr;
+    private final int levelUpPlusInt;
+    private final int levelUpPlusHP;
+    private final List<SkillInfo> skills = new ArrayList<>();
+
+    public PlayerCharacter(String name) {
+        super(name, 100, 50, 100, 10, 10, 0);
+        this.level = 1;
+        this.exp = 0;
+        this.maxExp = 100;
+        this.levelUpPlusInt = 10;
+        this.levelUpPlusHP = 10;
+        this.levelUpPlusStr = 5;
+        this.currentJob = new SwordMan();
+        updateStats(currentJob);
+    }
+
+    public void advanceJob(CharacterJob newJob) {
+        this.currentJob = newJob;
+        updateStats(newJob);
+        List<SkillInfo> skillsToLearn = newJob.getSkillList();
+        for(SkillInfo skills : skillsToLearn){
+            this.learnSkill(skills);
+        }
+    }
+
+    public void storeExp(int addExp){
+        if((this.exp + addExp) >= this.maxExp){
+            int dummyExp = (this.exp + addExp) - maxExp;
+            levelUp();
+            this.exp += dummyExp;
+        }
+        else{
+            this.exp += addExp;
+        }
+    }
+
+    public void learnSkill(SkillInfo skillToAdd) {
+        this.skills.add(skillToAdd);
+    }
+
+    private void updateStats(CharacterJob newJob) {
+        if (newJob.getJobName().equals("기사") || newJob.getJobName().equals("대검사") || newJob.getJobName()
+                .equals("용기사")) {
+            int addAttackDamage = this.currentJob.getAttackBonus();
+            addAttackDamage(addAttackDamage);
+        }
+
+        int addMagicForce = this.currentJob.getAttackBonus();
+        int addHp = this.currentJob.getHpBonus();
+        addMagicForce(addMagicForce);
+        addHP(addHp);
+    }
+
+    private void levelUp(){
+        this.level++;
+        this.exp = 0;
+        levelUpHp(levelUpPlusHP);
+        levelUpAttackDamage(levelUpPlusStr);
+        if(this.currentJob.getJobName().equals("성기사") || this.currentJob.getJobName().equals("성검사")){
+            levelUpMagicForce(levelUpPlusInt);
+        }
+    }
+}
