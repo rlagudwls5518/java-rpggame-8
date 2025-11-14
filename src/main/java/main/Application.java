3ab7df5feb73664357d579e40c433ed628ecdc20
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-14 20:00:51 +0900

    refactor: 애플리케이션 진입점 수정

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,50 +1,10 @@
 package main;
 
-import java.io.Console;
-import main.util.Clear;
-import main.view.StartOutputView;
+import main.controller.GameController;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
-        Console console;
-        String name = "김전사";
-        String job = "전사";
-        int level = 1;
-        int exp = 0;
-        int hp = 30;
-        int gold = 0;
-        String weapon = "낡은 목도";
-        String shield = "나무 방패(2)";
-
-        StartOutputView.showSplashScreen();
-        Clear.clearScreen();
-        StartOutputView.showPrologue();
-        Clear.clearScreen();
-        StartOutputView.showStartView();
-
-        for (int i = 0; i < 10; i++) {
-            Clear.clearScreen();
-            printStatus(name, job, level, exp, hp, gold, weapon, shield);
-            Thread.sleep(1000);
-
-            hp -= 5;
-            exp += 10;
-
-            if (hp <= 0) {
-                Clear.clearScreen();
-                System.out.println("-----------GAME OVER-----------");
-                break;
-            }
-        }
+        GameController gameController = new GameController();
+        gameController.run();
     }
-
-    public static void printStatus(String name, String job, int level, int exp, int hp, int gold, String weapon, String shield) {
-        System.out.println("-----------□게임 월드□-----------");
-        System.out.printf("%-6s : %-10s   %s : %s\n", "이름", name, "클래스", job);
-        System.out.printf("%-6s : %-10d   %s : %d\n", "LV", level, "EXP", exp);
-        System.out.printf("%-6s : %-10d   %s : %d\n", "HP", hp, "골드", gold);
-        System.out.printf("%-6s : %-10s   %s : %s\n", "무기", weapon, "방패", shield);
-        System.out.println("------------------------------------");
-    }
-
 }
