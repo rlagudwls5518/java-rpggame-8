01944a8ae6132a2e17ab2c261fd693e889c4317f
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-16 16:02:57 +0900

    refactor: 컨벤션 코드 수정

--- src/main/java/main/model/unit/character/CharacterJob.java
+++ src/main/java/main/model/unit/character/CharacterJob.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 public interface CharacterJob {
     String getJobName();
+
     List<Skill> getSkillList();
+
     int getHpBonus();
+
     int getAttackBonus();
+
     boolean canAdvance(int currentLevel);
+
     CharacterJob advance(int choice); //선택에 따라 확정된 다음 직업을 반환
+
     List<CharacterJob> getAdvancementOptions();//전직 가능한 다음 직업 목록을 반환
+
     int getJobAdvancementTier();
 
 }
