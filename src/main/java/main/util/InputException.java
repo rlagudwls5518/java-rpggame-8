6eb1274cd039dcfe323f68591ec91d851787d7ed
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-19 19:24:35 +0900

    refactor: 입력 예외처리 메시지 추가

--- src/main/java/main/util/InputException.java
+++ src/main/java/main/util/InputException.java
@@ -1,11 +1,14 @@
 package main.util;
 
 public class InputException {
+
+    private static final String NOT_INPUT_NUMBER = "숫자가 아닙니다";
+
     public static void inputException(String inputNum) {
         try {
             Integer.parseInt(inputNum);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_INPUT_NUMBER);
         }
     }
 }
